a8c727eaf234f987f63bacc1672b2fc3c538c950
Ian Kim - developer, Seungmin song - designer <lkim17@ucsc.edu>
2023-09-30 02:22:14 +0900

    push

--- Angular_basic/src/app/app.module.ts
+++ Angular_basic/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
